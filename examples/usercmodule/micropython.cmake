557f0b5c6f93b0ae67b61f3f3962ef4d59d678f9
TGiles1998 <90575567+TGiles1998@users.noreply.github.com>
2022-07-19 16:25:01 +0100

    remove CPP example

--- examples/usercmodule/micropython.cmake
+++ examples/usercmodule/micropython.cmake
@@ -7,5 +7,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cexample/micropython.cmake)
 
 # Add the CPP example.
-include(${CMAKE_CURRENT_LIST_DIR}/cppexample/micropython.cmake)
+#include(${CMAKE_CURRENT_LIST_DIR}/cppexample/micropython.cmake)
 
